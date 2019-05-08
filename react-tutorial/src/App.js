ed5c2afef108845b85809925cc7b90b1e7e91586
Prem Tiwari <prem.tiwari@news.co.uk>
2019-05-08 10:34:57 +0530

    Learning Reactjs JSX

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -8,17 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {HelloWorld()}
-        <h3>I am Prem</h3>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
       </header>
     </div>
   );
@@ -26,9 +15,23 @@ function App() {
 
 export default App;
 
-//Return the Hello World!
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Prem',
+  lastName: 'Tiwari'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
 function HelloWorld() {
-  return (
-    <h1>Hello World!</h1>
+  return(
+    element
   );
 }
